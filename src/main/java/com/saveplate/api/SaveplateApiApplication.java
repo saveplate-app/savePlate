3d83cf31116511a519796aa527686fd34b29ecee
saadboumahdi <saadbooumahdi@gmail.com>
2026-09-07 12:42:10 +0100

    putting the scafold of the project

---
+++ src/main/java/com/saveplate/api/SaveplateApiApplication.java
@@ -0,0 +1,13 @@
+package com.saveplate.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveplateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveplateApiApplication.class, args);
+    }
+
+}
